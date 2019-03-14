383c8eb688be9f563d357106c651d868bde2c60b
Siuming93 <2457944792@qq.com>
2019-03-14 12:30:22 +0800

    增加服务端信息

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.get("/orderCallBack", (req, res) => {
     }*/
     console.log("req.query:" + req.query);
     console.log("res:" + res);
-    arr.push("query.app:" + req.query.app + " uid" + req.query.uid + " serverSign" + getServerSign(req.query)) ;
+    arr.push("query.app:" + req.query.app + " uid" + req.query.uid + " serverSign" + getServerSign(req.query) + "reqSign" + req.query.sign) ;
     res.send("SUCCESS " + arr.length);
 
     return;
