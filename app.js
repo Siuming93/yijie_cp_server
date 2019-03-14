4c0420f2a29d9f223928052c773f51423dda953b
Siuming93 <2457944792@qq.com>
2019-03-14 12:33:38 +0800

    tostrng

--- app.js
+++ app.js
@@ -42,6 +42,7 @@ app.get("/orderCallBack", (req, res) => {
     }*/
     console.log("req.query:" + req.query);
     console.log("res:" + res);
+    console.log("server1:" + getServerSign1(req.query).toString());
     arr.push("query.app:" + req.query.app + " uid" + req.query.uid + " serverSign" + getServerSign(req.query) + "reqSign" + req.query.sign) ;
     res.send("SUCCESS " + arr.length);
 
